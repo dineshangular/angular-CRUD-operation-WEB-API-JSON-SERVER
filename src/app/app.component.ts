9429a9801273dc7792c13adcadef7d18ffebda68
dineshkarki <dineshkumar.attur@gmail.com>
2021-06-21 19:56:39 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonService } from './common.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'crud-operation-api';
+  allUsers:object;
+  userObj:any= {
+    id:'',
+    name:'',
+    mobile:'',
+    email:'',
+    position:''
+  }
+  isEdit:boolean = false
+  constructor(private commonService:CommonService){}
+  ngOnInit() {
+    this.getLatestUser();
+  }
+  addUser(formObj) {
+    this.commonService.createUser(formObj).subscribe((response)=>{
+      this.getLatestUser()
+    })
+    this.userObj={}
+  }
+  getLatestUser() {
+    this.commonService.getAllUser().subscribe((response)=>{
+      this.allUsers = response
+    })
+  }
+  editUser(user) {
+    this.isEdit = true;
+    this.userObj = user;
+  }
+  updateUser() {
+    this.isEdit = !this.isEdit;
+    this.commonService.updateUser(this.userObj).subscribe((response)=>{
+      this.getLatestUser();
+    })
+    this.userObj={}
+  }
+  deleteUser(user) {
+    this.commonService.deleteUser(user).subscribe(()=>{
+      this.getLatestUser();
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommonService } from './common.service';
 
 @NgModule({
   declarations: [
@@ -10,9 +12,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [CommonService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
